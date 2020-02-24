0909d8ccef4adfa1e9eda98e1935695736f10e47
Farras03 <faris200405@gmail.com>
2020-02-24 09:35:49 +0700

    test

---
+++ index.php
@@ -0,0 +1,79 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <meta http-equiv="X-UA-Compatible" content="ie=edge">
+    <title>Document</title>
+</head>
+<body>
+   <center> <h1>RESTORAN MULUK</h1>
+    <p>Isilah data dibawah ini</p>
+
+    <form action="resto.php" method="GET">
+        <input type="text" name="nama_lengkap" placeholder="Nama_lengkap">
+        <input type="text" name="alamat" placeholder="Alamat">
+        <input type="text" name="pesanan" placeholder="Pesanan">
+        <input type="text" name="no_telepon" placeholder="No_telepon">
+
+        <button type ="submit">Submit</button>
+    
+    </form>
+    </center>
+
+    <p>
+    </p>
+    
+    <h4 align="left" hspace="20" vspace="20">MENU MAKANAN</h4>
+    <p>
+        </p>
+        <img src="pecel.jpg" alt="menu" style="float:left;width:200px;height:150px;">
+        <br>
+        <h4 align="left" hspace="20" vspace="20">Pecel Enak Malang</h4>
+        <br>
+
+     <p>
+        </p>
+        <img src="lalapan.JPG" alt="menu" style="float:right;width:200px;height:150px;">
+        <br>
+        <h4 align="right" hspace="20" vspace="20">Lalapan Ayam </h4>
+        <br>
+   
+     <p>
+        </p>
+        <img src="bacem.jpg" alt="menu" style="float:left;width:200px;height:150px;">
+        <br>
+        <h4 align="left" hspace="20" vspace="20">Bacem Tempe</h4>
+        <br>
+
+    <p>
+        </p>
+        <img src="rawon.jpg" alt="menu" style="float:right;width:200px;height:150px;">
+        <br>
+        <h4 align="right" hspace="20" vspace="20">Rawon Sedap </h4>
+        <br>
+
+    <p>
+        </p>
+        <img src="nasgor.jpg" alt="menu" style="float:left;width:200px;height:150px;">
+        <br>
+        <h4 align="left" hspace="20" vspace="20">Nasgor Seafood</h4>
+        <br>
+
+    <p>
+        </p>
+        <img src="sate.jpg" alt="menu" style="float:right;width:200px;height:150px;">
+        <br>
+        <h4 align="right" hspace="20" vspace="20">Sate Ayam Madura </h4>
+        <br>
+    
+    <p>
+        </p>
+        <img src="rendang.jpg" alt="menu" style="float:left;width:200px;height:150px;">
+        <br>
+        <h4 align="left" hspace="20" vspace="20">Rendang Beef</h4>
+        <br>
+
+   
+</body>
+</html>
---
+++ resto.php
@@ -0,0 +1,76 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <meta http-equiv="X-UA-Compatible" content="ie=edge">
+    <title>Resto</title>
+</head>
+<body>
+<table>
+        <tr>
+        <td>Nama</td> <td>:<?php echo $_GET['nama_lengkap'] ?? '' ?></td>
+        </tr>
+        <tr>
+        <td>Alamat</td> <td>:<?php echo $_GET['alamat'] ?? '' ?></td>
+        </tr>
+        <tr>
+        <td>Pesanan</td> <td>:<?php echo $_GET['pesanan'] ?? '' ?></td>
+        </tr>
+        <tr>
+        <td>no_telepon</td> <td>:<?php echo $_GET['telepon'] ?? '' ?></td>
+        </tr>
+    </table>
+    <p>
+        </p>
+        <img src="pecel.jpg" alt="menu" style="float:left;width:200px;height:150px;">
+        <br>
+        <h4 align="left" hspace="20" vspace="20">Pecel Enak Malang</h4>
+        <br>
+
+     <p>
+        </p>
+        <img src="lalapan.JPG" alt="menu" style="float:right;width:200px;height:150px;">
+        <br>
+        <h4 align="right" hspace="20" vspace="20">Lalapan Ayam </h4>
+        <br>
+   
+     <p>
+        </p>
+        <img src="bacem.jpg" alt="menu" style="float:left;width:200px;height:150px;">
+        <br>
+        <h4 align="left" hspace="20" vspace="20">Bacem Tempe</h4>
+        <br>
+
+    <p>
+        </p>
+        <img src="rawon.jpg" alt="menu" style="float:right;width:200px;height:150px;">
+        <br>
+        <h4 align="right" hspace="20" vspace="20">Rawon Sedap </h4>
+        <br>
+
+    <p>
+        </p>
+        <img src="nasgor.jpg" alt="menu" style="float:left;width:200px;height:150px;">
+        <br>
+        <h4 align="left" hspace="20" vspace="20">Nasgor Seafood</h4>
+        <br>
+
+    <p>
+        </p>
+        <img src="sate.jpg" alt="menu" style="float:right;width:200px;height:150px;">
+        <br>
+        <h4 align="right" hspace="20" vspace="20">Sate Ayam Madura </h4>
+        <br>
+    
+    <p>
+        </p>
+        <img src="rendang.jpg" alt="menu" style="float:left;width:200px;height:150px;">
+        <br>
+        <h4 align="left" hspace="20" vspace="20">Rendang Beef</h4>
+        <br>
+
+
+
+</body>
+</html>
